c872d4d753d0620e28cca4d72c32ec5db593b213
whitis-purva <nicolewhitispurva@gmail.com>
2026-07-10 17:28:37 -0400

    Updates

--- _next/static/DTY3X_UtSVZoS1QuSPJvX/_buildManifest.js
+++ _next/static/DTY3X_UtSVZoS1QuSPJvX/_buildManifest.js
@@ -9,7 +9,7 @@ self.__BUILD_MANIFEST = {
     "./static/chunks/16299a86ecb00a0d.js"
   ],
   "/pitch/[slug]": [
-    "./static/chunks/9d19573be0efa8ed.js"
+    "./static/chunks/dbdad9abab618424.js"
   ],
   "/projects/[slug]": [
     "./static/chunks/8f4b9463a63087ab.js"
--- _next/static/chunks/dbdad9abab618424.js
+++ _next/static/chunks/dbdad9abab618424.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/pitch/[slug]", [
-  "static/chunks/065350442f5b330a.js",
+  "static/chunks/382dfe8aa108dfbc.js",
   "static/chunks/3d1862bf06b3a24d.js",
   "static/chunks/1e896674476cdc9e.js",
-  "static/chunks/turbopack-9249a5eb75f63b8e.js"
+  "static/chunks/turbopack-6b840fc35ccd8a20.js"
 ])
